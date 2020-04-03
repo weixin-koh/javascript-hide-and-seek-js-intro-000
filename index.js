344f5a0f8c44a6e9edd1c281a0e448d72ec4c4b8
Wei Xin Koh <e0201350@u.nus.edu>
2020-04-03 10:55:08 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
 
+function nestedTarget() {
+  return document.querySelector("#nested").querySelector(".target");
+}
+
+function increaseRankBy(n) {
+  var ranks = document.querySelectorAll(".ranked-list > li");
+
+  for (let i = 0; i < ranks.length; i++) {
+    var value = parseInt(ranks[i].innerHTML);
+    ranks[i].innerHTML = value + n;
+  }
+}
+
+function deepestChild() {
+  var node = document.querySelector("div#grand-node");
+
+  while (node.querySelector("div") != null) {
+    node = node.querySelector("div");
+  }
+
+  return node;
+}
